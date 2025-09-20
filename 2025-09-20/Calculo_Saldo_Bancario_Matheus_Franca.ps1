f2713fe468d71d4d3ce862a4809c7b9a429b6092
MATHEUS DE FRANCA TORRES <teteupoxinha@gmail.com>
2025-09-20 10:07:48 -0300

    Criação do exercicio 02

---
+++ 2025-09-20/Calculo_Saldo_Bancario_Matheus_Franca.ps1
@@ -0,0 +1,9 @@
+Clear-Host
+
+[double]$saldoInicial = Read-Host "DIGITE O VALOR DO SEU SALDO NA CONTA: "
+[double]$valorDeposito = Read-Host "DIGITE O VALOR DO DEPOSITO: "
+[double]$valorSaque = Read-Host "DIGITE O VALOR DO SAQUE: "
+
+if $valorSaque < 0:
+    Write-Host "O VALOR DO SAQUE TEM QUE SER ACIMA DE ZERO!"
+    
