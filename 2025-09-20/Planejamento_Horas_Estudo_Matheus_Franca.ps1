a17b414cffe0072d227ab388572bd50548a1f2ab
MATHEUS DE FRANCA TORRES <teteupoxinha@gmail.com>
2025-09-20 11:08:53 -0300

    Novo exercicio

---
+++ 2025-09-20/Planejamento_Horas_Estudo_Matheus_Franca.ps1
@@ -0,0 +1,6 @@
+[int]$horasDisponiveis = Read-Host "QUANTAS HORAS VOCÊ TEM DISPONIVEIS DURANTE A SEMANA" 
+[int]$numeroDisciplinas = Read-Host "QUANTAS DISCIPLINAS VOCÊ TEM"
+[int]$horasDedicadas = $horasDisponiveis / $numeroDisciplinas
+[int]$sobraHoras = $horasDisponiveis % $horasDedicadas
+
+Write-Host "$horasDedicadas e $horasDisponiveis"
