5df5758c8cefd6cde972da524d4c3cdc712acbb4
MATHEUS DE FRANCA TORRES <teteupoxinha@gmail.com>
2025-09-20 09:41:51 -0300

    Finalizado

--- 2025-09-20/Exercicio01-OS-Matheus-Franca.ps1
+++ 2025-09-20/Exercicio01-OS-Matheus-Franca.ps1
@@ -2,3 +2,7 @@ Clear-Host
 
 $estoqueAtual = Read-Host "DIGITE O QUANTIDADE DO SEU ESTOQUE ATUAL: "
 $produtosVendidos = Read-Host "DIGITE A QUANTIDADE DE PRODUTOS VENDIDOS: "
+
+$estoqueFinal = $estoqueAtual - $produtosVendidos
+
+Write-Host "O VALOR DO SEU ESTOQUE APÓS AS VENDAS É: $estoqueFinal"
